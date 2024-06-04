6683ff2fc3e7e197f0c459738bf7e9468e539b24
Zac Fair <12246663+tuun@users.noreply.github.com>
2024-06-04 15:30:19 -0700

    updated tests logic

--- src/ConditionTests.ts
+++ src/ConditionTests.ts
@@ -8,7 +8,10 @@ const tests = {
     return value !== null;
   },
   NOT_EMPTY: (value: any) => {
-    return value !== "" && (Array.isArray(value) ? value.length > 0 : true);
+    return tests.NOT_NULL(value) && value !== "" && 
+    (tests.IS_ARRAY(value) ? value.length > 0 : true) && 
+    (tests.IS_OBJECT(value) ? !tests.IS_EMPTY_OBJECT(value) : true)
+    ;
   },
   NOT_FALSE: (value: any) => {
     return value !== false;
@@ -28,28 +31,76 @@ const tests = {
   IS_OBJECT: (value: any) => {
     return typeof value === 'object' && !Array.isArray(value) && value !== null;
   },
+
+  /**
+   * Thanks to https://stackoverflow.com/questions/679915/how-do-i-test-for-an-empty-javascript-object
+   */
+  IS_EMPTY_OBJECT: (value: any) => {
+    
+    if (!tests.IS_OBJECT(value)) {
+      return false;
+    }
+  
+    const proto = Object.getPrototypeOf(value);
+  
+    // consider `Object.create(null)`, commonly used as a safe map
+    // before `Map` support, an empty object as well as `{}`
+    if (proto !== null && proto !== Object.prototype) {
+      return false;
+    }
+
+    for (const prop in value) {
+      if (Object.prototype.hasOwnProperty.call(value, prop)) {
+        return false;
+      }
+    }
+  
+    return true;
+  },
   
   IS_FUNCTION: (value: any) => {
     return typeof value === "function";
   },
+
   /**
    * Checks if the value given IS NOT undefined, empty, null or false
    */
   IS_SET: (value: any) => {
     return tests.NOT_UNDEFINED(value) && tests.NOT_NULL(value) && tests.NOT_EMPTY(value) && tests.NOT_FALSE(value);
   },
+
   /**
    * Checks if the value given is undefined, empty, null or false
    */
   IS_NOT_SET: (value: any) => {
     return !tests.IS_SET(value);
   },
+
   /**
    * Checks if the value given is equal to a test value
    */
   EQUALS: (value: any, testValue: any) => {
-    return tests.IS_SET(value) && tests.IS_SET(testValue) && value.toString() === testValue.toString();
+
+    if (tests.IS_NUMBER(value) && tests.IS_NUMBER(testValue) {
+      return Number(value) === Number(testValue);
+    }
+
+    if (tests.IS_STRING(value) && tests.IS_STRING(testValue)) {
+      return value === testValue;
+    }
+
+    if (tests.IS_ARRAY(value) && tests.IS_ARRAY(testValue)) {
+      return JSON.stringify(value) === JSON.stringify(testValue);
+    }
+
+    if (tests.IS_OBJECT(value) && tests.IS_OBJECT(testValue)) {
+      return JSON.stringify(value) === JSON.stringify(testValue);
+    }
+
+    return value === testValue;
+
   },
+
   /**
    * Checks if the value given is not equal to a test value
    */
@@ -60,7 +111,10 @@ const tests = {
    * Checks if the value given contains a test value
    */
   CONTAINS: (value: any, testValue: any) => {
-    return tests.IS_STRING(value) && value.includes(testValue);
+    return (
+      tests.IS_STRING(value) ||
+      tests.IS_ARRAY(value)
+    ) && value.includes(testValue);
   },
   /**
    * Checks if the value given does not contain a test value
